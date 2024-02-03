5ed7f1367cf8ef62e06b5233138862f123f07ea7
ubiquitous-o <ooo.ubi.ooo@gmail.com>
2024-02-03 16:07:13 +0900

    init

---
+++ pub.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt     # MQTTのライブラリをインポート
+from time import sleep              # 3秒間のウェイトのために使う
+import argparse
+
+# ブローカーに接続できたときの処理
+def on_connect(client, userdata, flag, rc):
+  print("Connected with result code " + str(rc))
+
+# ブローカーが切断したときの処理
+def on_disconnect(client, userdata, rc):
+  if rc != 0:
+     print("Unexpected disconnection.")
+
+# publishが完了したときの処理
+def on_publish(client, userdata, mid):
+  print("publish: {0}".format(mid))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MQTT Publisher')
+    parser.add_argument('--host', type=str, default='localhost', help='MQTT Broker host')
+    parser.add_argument('--port', type=int, default=1883, help='MQTT Broker port')
+    parser.add_argument('--topic', type=str, default='arm/left', help='MQTT Topic')
+    parser.add_argument('--msg', type=str, default='Hello, World!', help='MQTT Message')
+    args = parser.parse_args()
+
+    client = mqtt.Client()                 # クラスのインスタンス(実体)の作成
+    client.on_connect = on_connect         # 接続時のコールバック関数を登録
+    client.on_disconnect = on_disconnect   # 切断時のコールバックを登録
+    client.on_publish = on_publish         # メッセージ送信時のコールバック
+
+    client.connect(args.host, args.port, 60)  # 接続先は自分自身
+    
+    # 通信処理スタート
+    client.loop_start()    # subはloop_forever()だが，pubはloop_start()で起動だけさせる
+
+    client.publish(args.topic, args.msg)    # トピック名とメッセージを決めて送信
---
+++ sub.py
@@ -0,0 +1,34 @@
+import paho.mqtt.client as mqtt
+import argparse
+ 
+# ブローカーに接続できたときの処理
+def on_connect(client,userdata, flag, rc, topic):
+  print("Connected with result code " + str(rc))  # 接続できた旨表示
+  client.subscribe(topic)  # subするトピックを設定 
+
+# ブローカーが切断したときの処理
+def on_disconnect(client, userdata, rc):
+  if  rc != 0:
+    print("Unexpected disconnection.")
+
+# メッセージが届いたときの処理
+def on_message(client, userdata, msg):
+  # msg.topicにトピック名が，msg.payloadに届いたデータ本体が入っている
+  print("Received message '" + str(msg.payload) + "' on topic '" + msg.topic + "' with QoS " + str(msg.qos))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='MQTT Subscriber')
+    parser.add_argument('--host', type=str, default='localhost', help='MQTT Broker host')
+    parser.add_argument('--port', type=int, default=1883, help='MQTT Broker port')
+    parser.add_argument('--topic', type=str, default='arm/left', help='MQTT Topic')
+    args = parser.parse_args()
+
+    # MQTTの接続設定
+    client = mqtt.Client()                 # クラスのインスタンス(実体)の作成
+    client.on_connect = on_connect(topic=args.topic)         # 接続時のコールバック関数を登録
+    client.on_disconnect = on_disconnect   # 切断時のコールバックを登録
+    client.on_message = on_message         # メッセージ到着時のコールバック
+    
+    client.connect(args.host, args.port, 60)  
+    
+    client.loop_forever()
