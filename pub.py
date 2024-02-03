5861b60d707fec0052b94ac8647341d5b9c7f958
ubiquitous-o <ooo.ubi.ooo@gmail.com>
2024-02-03 16:25:28 +0900

    tiny fix

--- pub.py
+++ pub.py
@@ -1,32 +1,49 @@
 import paho.mqtt.client as mqtt
 import argparse
+import random
 
-def on_connect(client, userdata, flag, rc):
-    print("Connected with result code " + str(rc))
+parser = argparse.ArgumentParser(description='MQTT Subscriber')
+parser.add_argument('--host', type=str, default='localhost', help='MQTT Broker host')
+parser.add_argument('--port', type=int, default=1883, help='MQTT Broker port')
+parser.add_argument('--topic', type=str, default='arm/left', help='MQTT Topic')
+args = parser.parse_args()
 
-def on_disconnect(client, userdata, rc):
-    if rc != 0:
-        print("Unexpected disconnection.")
+broker = args.host
+port = args.port
+topic = args.topic
 
-def on_publish(client, userdata, mid):
-    print("publish: {0}".format(mid))
+client_id = f'mqtt-{random.randint(0, 1000)}'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client):
+    msg_count = 0
+    msg = f"messages: {msg_count}"
+    result = client.publish(topic, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        print(f"Send `{msg}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='MQTT Publisher')
-    parser.add_argument('--host', type=str, default='localhost', help='MQTT Broker host')
-    parser.add_argument('--port', type=int, default=1883, help='MQTT Broker port')
-    parser.add_argument('--topic', type=str, default='arm/left', help='MQTT Topic')
-    parser.add_argument('--msg', type=str, default='Hello, World!', help='MQTT Message')
-    args = parser.parse_args()
-
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
-    client.on_publish = on_publish
-
-    client.connect(args.host, args.port, 60)
-
-    client.loop_start()
-
-    client.publish(args.topic, args.msg)
+    run()
--- sub.py
+++ sub.py
@@ -1,29 +1,47 @@
-import paho.mqtt.client as mqtt
+import random
 import argparse
- 
+from paho.mqtt import client as mqtt
 
-def on_connect(client,userdata, flag, rc, topic):
-    print("Connected with result code " + str(rc))
-    client.subscribe(topic) 
 
-def on_disconnect(client, userdata, rc):
-    if  rc != 0:
-        print("Unexpected disconnection.")
+parser = argparse.ArgumentParser(description='MQTT Subscriber')
+parser.add_argument('--host', type=str, default='localhost', help='MQTT Broker host')
+parser.add_argument('--port', type=int, default=1883, help='MQTT Broker port')
+parser.add_argument('--topic', type=str, default='arm/left', help='MQTT Topic')
+args = parser.parse_args()
 
-def on_message(client, userdata, msg):
-    print("Received message '" + str(msg.payload) + "' on topic '" + msg.topic + "' with QoS " + str(msg.qos))
+broker = args.host
+port = args.port
+topic = args.topic
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='MQTT Subscriber')
-    parser.add_argument('--host', type=str, default='localhost', help='MQTT Broker host')
-    parser.add_argument('--port', type=int, default=1883, help='MQTT Broker port')
-    parser.add_argument('--topic', type=str, default='arm/left', help='MQTT Topic')
-    args = parser.parse_args()
+client_id = f'mqtt-{random.randint(0, 1000)}'
 
-    client = mqtt.Client()
-    client.on_connect = on_connect(topic=args.topic)
-    client.on_disconnect = on_disconnect
+def connect_mqtt() -> mqtt:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def subscribe(client: mqtt):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
     client.on_message = on_message
-    client.connect(args.host, args.port, 60)  
 
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
     client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
